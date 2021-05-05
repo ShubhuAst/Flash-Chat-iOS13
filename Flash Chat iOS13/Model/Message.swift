ecbf0e47ad548543aa76afe98962d1186f8cbf9f
Shubham Asthana <asthana.shubham99@gmail.com>
2021-05-05 22:27:34 +0530

    1To1 chatting

---
+++ Flash Chat iOS13/Model/Message.swift
@@ -0,0 +1,14 @@
+//
+//  Message.swift
+//  Flash Chat iOS13
+//
+//  Created by SHUBHAM ASTHANA on 20/12/20.
+//  Copyright © 2020 Angela Yu. All rights reserved.
+//
+
+import Foundation
+
+struct Message {
+    let messageBody: String
+    let senderID: String
+}
---
+++ Flash Chat iOS13/Views/MessageCell.swift
@@ -0,0 +1,30 @@
+//
+//  MessageCell.swift
+//  Flash Chat iOS13
+//
+//  Created by SHUBHAM ASTHANA on 20/12/20.
+//  Copyright © 2020 Angela Yu. All rights reserved.
+//
+
+import UIKit
+
+class MessageCell: UITableViewCell {
+
+    @IBOutlet weak var messageBubble: UIView!
+    @IBOutlet weak var lable: UILabel!
+    @IBOutlet weak var rightImgView: UIImageView!
+    @IBOutlet weak var leftImgView: UIImageView!
+    
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        messageBubble.layer.cornerRadius = messageBubble.frame.size.height/5
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
